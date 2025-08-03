a304fadfee607058143e87df59fae39977fc025a
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-08-03 15:26:26 +0700

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -6,3 +6,5 @@
 # tar -czvf ../../output.tar.gz ../../output
 rm output.tar.gz
 tar -czvf output.tar.gz output/IosevkaSs05QuasiProportional
+
+echo "COMMIT_MSG=compress IosevkaSs05QuasiProportional" >> "$GITHUB_ENV"
