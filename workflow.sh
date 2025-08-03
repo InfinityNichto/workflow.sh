6dc15d5a0194e7c21fcc7d870108ef555e64a648
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-08-03 15:21:25 +0700

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,6 +1,8 @@
-cd Iosevka/Iosevka-main
-sudo apt install npm ttfautohint
-npm install
-npm run build -- ttf::IosevkaSs05QuasiProportional
-mv -v dist/* ../../output
-tar -czvf ../../output.tar.gz ../../output
+# cd Iosevka/Iosevka-main
+# sudo apt install npm ttfautohint
+# npm install
+# npm run build -- ttf::IosevkaSs05QuasiProportional
+# mv -v dist/* ../../output
+# tar -czvf ../../output.tar.gz ../../output
+rm output.tar.gz
+tar -czvf output.tar.gz output/IosevkaSs05QuasiProportional
