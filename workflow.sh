d0e85d04db385cf14c662113cb53d3241b4f6aa6
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-08-03 14:19:46 +0700

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,6 +1,7 @@
 cd Iosevka/Iosevka-main
 sudo apt install npm ttfautohint
 npm install
-npm run build -- ttf::PurificationQuasiPropo
+npm run build -- ttcize
 mkdir ../../output
 mv -v dist/* ../../output
+tar -czvf output.tar.gz ../../output
