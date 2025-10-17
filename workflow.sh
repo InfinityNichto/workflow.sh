663b655947790d13bd7a237e5c43844518146a5d
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-10-17 07:53:04 +0700

    Compile TCC for Android
    Update NDK and TinyCC build process in workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,21 +1,22 @@
 if [ ! -e "android-ndk-r23c-linux.zip" ]; then
   wget https://dl.google.com/android/repository/android-ndk-r23c-linux.zip
   unzip android-ndk-r23c-linux -d ndk
-  ls -la ndk
-  ls -la ndk/*
 fi
 
-# if [ -e "tinycc" ]; then
-#   rm -rf tinycc
-#   git rm --cached tinycc
-# fi
+export CC="./ndk/23.2.8568313/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang"
+export AR="./ndk/23.2.8568313/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar"
 
-# git clone https://github.com/TinyCC/tinycc
-# cd tinycc
-# rm -rf .git
+if [ -e "tinycc" ]; then
+  rm -rf tinycc
+  git rm --cached tinycc
+fi
 
-# sudo apt install build-essential
+git clone https://github.com/TinyCC/tinycc
+cd tinycc
+rm -rf .git
 
-# make clean
-# ./configure
-# make
+sudo apt install build-essential
+
+make clean
+./configure --cc=$CC --ar=$AR --cpu=arm64 --extra-cflags="-fPIC"
+make
