923088ccb566ec2389eb72487be1004ee451acce
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-10-17 07:43:05 +0700

    Setup NDK r23c

--- workflow.sh
+++ workflow.sh
@@ -1,14 +1,21 @@
-if [ -e "tinycc" ]; then
-  rm -rf tinycc
-  git rm --cached tinycc
+if [ ! -e "android-ndk-r23c-linux.zip" ]; then
+  wget https://dl.google.com/android/repository/android-ndk-r23c-linux.zip
+  unzip android-ndk-r23c-linux -d ndk
+  ls -la ndk
+  ls -la ndk/*
 fi
 
-git clone https://github.com/TinyCC/tinycc
-cd tinycc
-rm -rf .git
+# if [ -e "tinycc" ]; then
+#   rm -rf tinycc
+#   git rm --cached tinycc
+# fi
 
-sudo apt install build-essential
+# git clone https://github.com/TinyCC/tinycc
+# cd tinycc
+# rm -rf .git
 
-make clean
-./configure
-make
+# sudo apt install build-essential
+
+# make clean
+# ./configure
+# make
