5c112566299300de68006b8016ee5fcadc4d8671
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-10-17 08:49:52 +0700

    Actually set the correct path for CC and AR
    Updated NDK paths and improved cleanup logic in workflow.sh.

--- workflow.sh
+++ workflow.sh
@@ -3,25 +3,26 @@ if [ ! -e "android-ndk-r23c-linux.zip" ]; then
   unzip android-ndk-r23c-linux -d ndk
 fi
 
-# export CC="./ndk/23.2.8568313/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang"
-# export AR="./ndk/23.2.8568313/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar"
+export CC="./ndk/android-ndk-r23c/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang"
+export AR="./ndk/android-ndk-r23c/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar"
 
-# if [ -e "tinycc" ]; then
-#   rm -rf tinycc
-#   git rm --cached tinycc
-# fi
+if [ -e "tinycc" ]; then
+  rm -rf tinycc
+  git rm --cached tinycc
+fi
 
-# git clone https://github.com/TinyCC/tinycc
-# cd tinycc
-# rm -rf .git
+git clone https://github.com/TinyCC/tinycc
+cd tinycc
+rm -rf .git
 
-# sudo apt install build-essential
+sudo apt install build-essential
 
-# make clean
-# ./configure --cc=$CC --ar=$AR --cpu=arm64 --extra-cflags="-fPIC"
-# make
+make clean
+./configure --cc=$CC --ar=$AR --cpu=arm64 --extra-cflags="-fPIC"
+make
 
-sudo apt install tree
-tree ndk 
+# sudo apt install tree
+# tree ndk/android-ndk-r23c
 
-rm -rf ndk # too lazy to add untrack feature, just delete the entire thing before automatic track
+# here to prevent triggering max push limit (100MB)
+rm -rf ndk android-ndk-r23c-linux.zip # too lazy to add untrack feature, just delete the entire thing before automatic track
