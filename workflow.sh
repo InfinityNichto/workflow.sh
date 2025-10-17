b5390ab0a94ba3105c253bd6923599c14b742189
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-10-17 07:23:34 +0700

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,5 +1,8 @@
-# Download the Unity tarball for Linux
-wget https://download.unity3d.com/download_unity/4016570cf34f/LinuxEditorInstaller/Unity.tar.xz
+git clone https://github.com/TinyCC/tinycc
+cd tinycc
 
-# Extract only il2cpp
-tar -xf Unity.tar.xz --wildcards "*/il2cpp/*" --strip-components=1
+sudo apt install build-essential
+
+make clean
+./configure
+make
