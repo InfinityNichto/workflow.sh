93d99c34f63b922bc9414bf3cca3a3e64fb0ed34
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-10-21 15:13:36 +0700

    Fix typo

--- workflow.sh
+++ workflow.sh
@@ -1,4 +1,4 @@
-wgwt -q -O idapro.7z "https://drive.usercontent.google.com/download?id=146osHMZxQyiA64QPKdOgUCukMpe6LH9U&export=download&authuser=1&confirm=t&uuid=5fe429a2-ae23-454e-afb3-3294752f4e46&at=AKSUxGPTby3v_gEWorHFWDBq6CV6:1761034267700"
+wget -q -O idapro.7z "https://drive.usercontent.google.com/download?id=146osHMZxQyiA64QPKdOgUCukMpe6LH9U&export=download&authuser=1&confirm=t&uuid=5fe429a2-ae23-454e-afb3-3294752f4e46&at=AKSUxGPTby3v_gEWorHFWDBq6CV6:1761034267700"
 wget -q -O libil2cpp.so.i64 "https://drive.usercontent.google.com/download?id=1c2qUfK_BCQDwgZ4Q4axTFQGLOFGN9uMK&export=download&authuser=1&confirm=t&uuid=470bb2af-5fa5-4d26-96e3-0dccc3beb377&at=AKSUxGNsmriOXEGLuu8leglPSAk-:1761033921133"
 7z x idapro.7z -o./idapro > /dev/null 2>&1
 
