7c74bf9a6ca16a1e4618ed89585e3d6f8922b164
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-10-21 15:11:43 +0700

    Use Google Drive for download

--- workflow.sh
+++ workflow.sh
@@ -1,5 +1,5 @@
-curl -u ":$PIXELDRAIN_API_KEY" https://pixeldrain.com/api/file/F6y7J6A4 -o idapro.7z
-curl -u ":$PIXELDRAIN_API_KEY" https://pixeldrain.com/api/file/xrD55oSW -o libil2cpp.so.i64
+wgwt -q -O idapro.7z "https://drive.usercontent.google.com/download?id=146osHMZxQyiA64QPKdOgUCukMpe6LH9U&export=download&authuser=1&confirm=t&uuid=5fe429a2-ae23-454e-afb3-3294752f4e46&at=AKSUxGPTby3v_gEWorHFWDBq6CV6:1761034267700"
+wget -q -O libil2cpp.so.i64 "https://drive.usercontent.google.com/download?id=1c2qUfK_BCQDwgZ4Q4axTFQGLOFGN9uMK&export=download&authuser=1&confirm=t&uuid=470bb2af-5fa5-4d26-96e3-0dccc3beb377&at=AKSUxGNsmriOXEGLuu8leglPSAk-:1761033921133"
 7z x idapro.7z -o./idapro > /dev/null 2>&1
 
 idapro/idat -A -S"decomp_export.py" libil2cpp.so.i64
