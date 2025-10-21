a0f1d4c9977274740314f504868b09cc55bb7187
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-10-21 13:44:38 +0700

    test idat script run

--- workflow.sh
+++ workflow.sh
@@ -1,11 +1,14 @@
 wget -q "https://pixeldrain.com/api/file/F6y7J6A4?download"
+wget -q "https://pixeldrain.com/api/file/xrD55oSW?download"
 mv "F6y7J6A4?download" idapro.7z
-7z x idapro.7z -o./idapro
+mv "xrD55oSW?download" libil2cpp.so.i64
+7z x idapro.7z -o./idapro > /dev/null 2>&1
 
-idapro/idat --help
+idapro/idat -A -S"decomp_export.py" libil2cpp.so.i64
 
 rm -rf idapro
 rm idapro.7z
+rm libil2cpp.so.i64
 
 # echo "COMMIT_MSG=message" >> "$GITHUB_ENV"
 echo "COMMIT_MSG=idapro on CI test" >> "$GITHUB_ENV"
