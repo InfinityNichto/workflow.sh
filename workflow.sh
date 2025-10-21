759d7ee593c52e3831740de3fd0db67f128bd8b1
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-10-21 14:02:30 +0700

    Use Pixeldrain API key for download

--- workflow.sh
+++ workflow.sh
@@ -1,7 +1,5 @@
-wget -q "https://pixeldrain.com/api/file/F6y7J6A4?download"
-wget -q "https://pixeldrain.com/api/file/xrD55oSW?download"
-mv "F6y7J6A4?download" idapro.7z
-mv "xrD55oSW?download" libil2cpp.so.i64
+curl -u ":$PIXELDRAIN_API_KEY" https://pixeldrain.com/api/file/F6y7J6A4 -o idapro.7z
+curl -u ":$PIXELDRAIN_API_KEY" https://pixeldrain.com/api/file/xrD55oSW -o libil2cpp.so.i64
 7z x idapro.7z -o./idapro > /dev/null 2>&1
 
 idapro/idat -A -S"decomp_export.py" libil2cpp.so.i64
