40623720e68f3503d88ea1f4bbec316df6529b4a
ничто (alissa) <119140957+InfinityNichto@users.noreply.github.com>
2025-10-21 13:10:29 +0700

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,6 +1,11 @@
 wget "https://pixeldrain.com/api/file/F6y7J6A4?download"
-file "F6y7J6A4?download"
-rm "F6y7J6A4?download"
+mv "F6y7J6A4?download" idapro.7z
+7z x idapro.7z -o./idapro
+
+idapro/idat --help
+
+rm -rf idapro
+rm idapro.7z
 
 # echo "COMMIT_MSG=message" >> "$GITHUB_ENV"
 echo "COMMIT_MSG=idapro on CI test" >> "$GITHUB_ENV"
